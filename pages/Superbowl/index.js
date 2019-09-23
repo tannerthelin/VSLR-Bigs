9736dcf12d2b4fc25b873b51c18f32a6c3e47b01
Tanner Thelin <tannerthelin@tanners-macbook.gosolarit.com>
2019-09-23 11:05:14 -0600

    Adding Superbowl page

---
+++ pages/Superbowl/index.js
@@ -0,0 +1,48 @@
+function init() {
+Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1wbONXqwGphaZEjbOfMMDC67XDyYuwtHrk2XI2-DD5Yg/edit?usp=sharing',
+callback: function(data, tabletop) { 
+console.log(data)
+},
+simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+console.log(data);
+var counter = 1;
+
+// Change this variable to switch the office!
+var currentOffice = "Superbowl Tickets";
+document.getElementById("page-title").innerHTML = currentOffice;
+document.title = "Bigs -" + currentOffice;
+
+// The loop for the first guy
+for (var i = 0; i < 1; i++) {
+
+	$("#table-body").append(
+		'<tr class="border_bottom">' +
+		'<td class="number-column">' + counter + '</td>' +
+		'<td class="name-column">' + data[i].name + '</td>' +
+		'<td class="comp-column needed">' + data[i].bigs_score + '</td>' +
+		'<td class="league-column last">' + data[i].league_score + '</td>' +
+		'</tr>'
+		);
+	counter = counter + 1;
+	
+}
+
+// The loop for the rest of the guys
+for (var i = 1; i < 50; i++) {
+
+	$("#table-body").append(
+		'<tr class="border_bottom">' +
+		'<td class="number-column">' + counter + '</td>' +
+		'<td class="name-column dark-row">' + data[i].name + '</td>' +
+		'<td class="comp-column dark-row needed">' + data[i].bigs_score + '</td>' +
+		'<td class="league-column dark-row last">' + data[i].league_score + '</td>' +
+		'</tr>'
+		);
+	counter = counter + 1;
+	
+}
+
+}
